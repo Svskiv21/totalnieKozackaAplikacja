9839b2dc4ea2323ed9c1c393dbae8e70550801e4
Natalia Krukar <natalia.krukar@gmail.com>
2024-02-01 00:52:26 +0100

    dostosowanie wersji w konfiguracji zeby sie projekt budowal

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = "1.5.0"
     }
     packaging {
         resources {
@@ -58,6 +58,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
     implementation("androidx.room:room-runtime:2.6.1")
     ksp("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
--- app/src/main/java/com/example/totalniekozackaaplikacja/dataAccess/dao/PlayerWithScoresDao.kt
+++ app/src/main/java/com/example/totalniekozackaaplikacja/dataAccess/dao/PlayerWithScoresDao.kt
@@ -8,7 +8,7 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 abstract class PlayerWithScoresDao {
 
-    @Query("select players.player_id as id_profile, `score_id` as id_score, `points` as points," +
+    @Query("select players.player_id as player_id, `score_id` as score_id, `points` as points," +
             "players.name as name, players.email as email " +
             "from players, scores where players.player_id = scores.player_id")
     abstract fun getPlayersWithScore(): Flow<List<PlayerWithScores>>
